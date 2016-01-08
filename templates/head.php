4a4d82fb3cde9f2ec6cfe5b87a0129a7cd16e458
erikkowalski <erikkowalski@mac.com>
2016-01-08 15:43:21 -0500

    initial commit

---
+++ templates/head.php
@@ -0,0 +1,7 @@
+<head>
+	<link href='https://fonts.googleapis.com/css?family=Lato:400,700,300,400italic' rel='stylesheet' type='text/css'>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php wp_head(); ?>
+</head>
