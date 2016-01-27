7899cf14812b7ec56aab461450112cc9c56f138a
erikkowalski <erikkowalski@mac.com>
2016-01-27 14:32:54 -0500

    Add logic for container-fluid in base file and extra.phps

--- base.php
+++ base.php
@@ -18,7 +18,13 @@ use Roots\Sage\Wrapper;
       do_action('get_header');
       get_template_part('templates/header');
     ?>
-    <div class="wrap container" role="document">
+	  <?php
+	  $url = basename( Wrapper\template_path() );
+	  $full_width_pages = [ 'front-page.php', 'template-home.php', 'page-contact.php' ];
+	  $container = Roots\Sage\Extras\container_selector( $url, $full_width_pages );
+	  ?>
+
+	  <div class="wrap <?= $container ?>" role="document">
       <div class="content row">
         <main class="main">
           <?php include Wrapper\template_path(); ?>
