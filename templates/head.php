9d19386f244451adee7658332c77fbb7ab51de7f
erikkowalski <erikkowalski@mac.com>
2016-02-07 12:53:42 -0500

    Added Baskerville forom google fonts

--- templates/head.php
+++ templates/head.php
@@ -3,5 +3,6 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href='https://fonts.googleapis.com/css?family=Lato:400,700,300,400italic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Libre+Baskerville:400,700' rel='stylesheet' type='text/css'>
   <?php wp_head(); ?>
 </head>
