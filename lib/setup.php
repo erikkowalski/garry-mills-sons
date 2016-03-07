0a17643897c381296cb4ba50defedae12aa54ccb
erikkowalski <erikkowalski@mac.com>
2016-03-07 13:20:47 -0500

    Added admin bar fix, added featured images,

--- lib/setup.php
+++ lib/setup.php
@@ -85,6 +85,7 @@ function display_sidebar() {
     // @link https://codex.wordpress.org/Conditional_Tags
     is_404(),
     is_front_page(),
+	  is_home() ,
     is_page_template('template-custom.php'),
     is_page_template('page.php'),
     is_page_template('template-custom.php'),
