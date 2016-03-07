c0b055b6f40062868616a0542eaf55d014746d69
erikkowalski <erikkowalski@mac.com>
2016-03-07 13:29:26 -0500

    Added Our Work layout sytles

--- templates/content.php
+++ templates/content.php
@@ -1,12 +1,10 @@
-<article <?php post_class(); ?>>
+<article <?php post_class('col-md-6'); ?>>
   <header>
 	  <?php if ( has_post_thumbnail() ) {
 	the_post_thumbnail();
 }  ?>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php get_template_part('templates/entry-meta'); ?>
+
   </header>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
-  </div>
+
 </article>
