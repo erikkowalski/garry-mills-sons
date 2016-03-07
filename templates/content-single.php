ca562607744236f20f3ff88123670fbf1aa8ee3c
erikkowalski <erikkowalski@mac.com>
2016-03-07 15:51:45 -0500

    Added single.scss, added styling to single page

--- templates/content-single.php
+++ templates/content-single.php
@@ -40,9 +40,16 @@
 		  <?php // get_template_part('templates/entry-meta'); ?>
 	  </header>
 
-    <div class="entry-content">
+    <div class="entry-content clearfix">
       <?php the_content(); ?>
     </div>
+	  <nav class="post-nav">
+		  <ul class="pager">
+			  <li class="previous"><?php previous_post_link( '%link', '&larr; %title' ); ?></li>
+			  <li class="next"><?php next_post_link( '%link', '%title &rarr;' ); ?></li>
+		  </ul>
+	  </nav>
+
     <footer>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
     </footer>
